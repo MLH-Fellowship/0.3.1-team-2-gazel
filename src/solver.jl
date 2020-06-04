429a382abde7ea5d0f2e85e889182c118f54ddf1
Biswajit Ghosh <biswajitghosh98@iitkgp.ac.in>
2020-06-04 17:45:16 +0530

    added solver with binary search

--- src/solver.jl
+++ src/solver.jl
@@ -7,7 +7,7 @@ i0 = 0.5  #intial brightness
 a certain sensitivity value can be generated
 eg :  sol(115)
 =#
-s_span = (0.0,100.0)
+s_span = (0.0,10.0)
 #I've taken a value of 1/3.0; might have to change this after testing
 prob = ODEProblem(eqn,i0,s_span)
 sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8, dt=0.1, save_everystep=true)
@@ -17,3 +17,14 @@ On printing sol, you'll find 2 lists, one for t, one for u
 here t is s
 u is intensity
 =#
+arsol = Array(sol)
+println(arsol)        #solution array
+
+#############
+#This part should logically be in  a different script. Will break down while integration
+#############
+#I've assumed my current intensity to be 1.0, hence i computed its index
+searchsortedfirst(arsol,1.0) #result 12
+#Assumed required intensity to be 7.0 (Note : intensity can't be more than 10); will have to discuss solution handling
+searchsortedfirst(arsol,7.0) # result 34
+ #now iterate through 12 to 34 and change brightness accordingly
