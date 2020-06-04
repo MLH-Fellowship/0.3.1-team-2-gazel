7afe3c564a459339bb2d0b0ae2f7ee1568510c50
Gautam Mishra <mishragautam96@gmail.com>
2020-06-04 22:36:30 +0530

    add comments to gazel.jl

--- src/gazel.jl
+++ src/gazel.jl
@@ -8,13 +8,22 @@ include("control.jl")
 function __init__()
     #check_deps()
 
+    # Get the name of the display
+    display = _getDisplay()
+
+    # setup constants
     eqn(i,p,s) =  i * 0.3
     i0 = 0.5
     s_span = (0.0,10.0)
+
+    # setup problem
     prob = ODEProblem(eqn,i0,s_span)
+
+    # solve the ODE and convert the descretized soln into an array
     sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8, dt=0.1, save_everystep=true)
     arsol = Array(sol)
-    display = _getDisplay()
+    
+    # start the feed
     _start(display, arsol)
 
 end
