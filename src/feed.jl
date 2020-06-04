b094af2d779e75d762898c8643dd17467178eff2
Gautam Mishra <mishragautam96@gmail.com>
2020-06-04 14:24:27 +0530

    add image processing

--- src/feed.jl
+++ src/feed.jl
@@ -1,44 +1,41 @@
 using VideoIO, Images
-include("control.jl")
+
 
 """
-    getc1()
-
-Util function to quit the loop
-"""
-function getc1()
-    ret = ccall(:jl_tty_set_mode, Int32, (Ptr{Cvoid},Int32), stdin.handle, true)
-    ret == 0 || error("unable to switch to raw mode")
-    c = read(stdin, Char)
-    ccall(:jl_tty_set_mode, Int32, (Ptr{Cvoid},Int32), stdin.handle, false)
-    c
-end
-
-"""
-    getc1()
+    start()
 
 Main function which starts the video feed
     and calls subsequent functions
 """
 function start()
     cam = VideoIO.opencamera()
-    display = _getDisplay()
     n = 0
     while true
         n += 1
-        # opt = getc1();
-        # if opt == 'q'
-        #     break
-        # end
         if n%10 != 0
             continue
         end
-        rgb_img = read(cam)
-        hsl_img = HSL.(rgb_img)
-        channels = channelview(float.(hsl_img))
-        L = channels[3,:,:]
-        brightness = sum(L)/length(L)
+        rgbImg = read(cam)
+        singleChannel = processImage(rgbImg)
+        println(length(singleChannel))
     end
 end
 
-start()
+
+"""
+    processImage()
+
+Handles the image processing part which will
+be taken by contrast finder
+pType: Eithe gray or HSL with default value gray
+returns: A single channel matrix
+"""
+function processImage(img, pType="gray")
+    if pType == "gray"
+        return Gray.(img)
+    elseif pType == "HSL"
+        hslImg = HSL.(img)
+        channels = channelview(float.(hslImg))
+        return channels[3,:,:]
+    end
+end
