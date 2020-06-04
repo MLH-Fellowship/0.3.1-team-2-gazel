3127e00908a842e6973a1a00272c921a67364308
Biswajit Ghosh <biswajitghosh98@iitkgp.ac.in>
2020-06-04 17:27:20 +0530

    solver added (will improve with discrete callbacks if time permits)

--- src/solver.jl
+++ src/solver.jl
@@ -1,8 +1,8 @@
 using DifferentialEquations
+using DataStructures
 #here t denotes sensitivity, 0.3 is a constant which can be changed later after testing
 eqn(i,p,s) =  i * 0.3
-#I've taken u0 to as 0.0, whereas this should be the current intensity value
-i0 = 0.0
+i0 = 0.5  #intial brightness
 #=this would just generate a sol object from which any value of intensity corresponding to
 a certain sensitivity value can be generated
 eg :  sol(115)
@@ -10,12 +10,10 @@ eg :  sol(115)
 s_span = (0.0,100.0)
 #I've taken a value of 1/3.0; might have to change this after testing
 prob = ODEProblem(eqn,i0,s_span)
-sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8, dt=0.1)
+sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8, dt=0.1, save_everystep=true)
 print(sol)
 #=
 On printing sol, you'll find 2 lists, one for t, one for u
 here t is s
 u is intensity
 =#
-
-println(sol(3.0))
